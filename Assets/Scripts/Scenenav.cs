b2434cb624e56bd2887e05ec9cf895bd1ea0d8bc
RachitWalke <67267492+RachitWalke@users.noreply.github.com>
2021-04-19 17:20:33 +0530

    Menu Implementation

---
+++ Assets/Scripts/Scenenav.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Scenenav : MonoBehaviour {
+
+    public void sceneloader (string name)
+    {
+        SceneManager.LoadScene(name);
+    }
+    public void quit()
+    {
+        Application.Quit();
+    }
+
+
+}
